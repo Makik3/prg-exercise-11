4330c7398317328f036fc4dea69a65c06547510e
Marketa Klimankova <maki7@post.cz>
2026-04-22 10:52:23 +0200

    pokuus

--- sorting.py
+++ sorting.py
@@ -1,4 +1,6 @@
 import random
+import matplotlib.pyplot as plt
+
 
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
@@ -23,3 +25,37 @@ def selection_sort(values):
     return values
 print(selection_sort(values))
 
+
+def bubble_sort(seznam_cisel):
+    plt.ion()
+    plt.show()
+
+    for j in range(len(seznam_cisel)):
+        swapped = False
+        for i in range(1, len(seznam_cisel)):
+            index_highlight1 = i - 1
+            index_highlight2 = i
+            colors = ["steelblue"] * len(seznam_cisel)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(seznam_cisel)), seznam_cisel, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+            if seznam_cisel[i] < seznam_cisel[i-1]:
+                seznam_cisel[i], seznam_cisel[i-1] = seznam_cisel[i-1], seznam_cisel[i]
+                swapped = True
+        if swapped == False:
+            return seznam_cisel
+
+    plt.ioff()
+    plt.show()
+
+print(bubble_sort([42, 7, 91, 15, 63, 8, 57, 73, 2, 100]))
+
+
+if __name__ == "__main__":
+    values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+    print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
+
+    small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
